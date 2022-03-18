9b9f1a968142a5e46575f76e558f95b4a2a55e6b
NoahJeong92 <wjdxoghs@gmail.com>
2022-03-18 18:31:53 +0900

    회원 도메인 생성

---
+++ src/main/java/com/studyolle/studyolle/domain/Account.java
@@ -0,0 +1,52 @@
+package com.studyolle.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter@Setter @EqualsAndHashCode(of ="id") //연관관계가 복잡해질때, 무한루프 스택오버플로우 발생 할 것을 대비해서
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
